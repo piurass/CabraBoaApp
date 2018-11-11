da645885a84862297e086f574b0e2a46bd642866
Rogerio de Alencar Santos <piurass@yahoo.com.br>
2018-11-10 23:48:27 -0200

    Adicionar arquivos de projeto.

---
+++ CabraBoaApp/CabraBoaApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CabraBoaApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTappedCrescimento(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new PageCrescimento());
+        }
+
+        void OnTappedReproducao(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new PageReproducao());
+        }
+
+        void OnTappedCadastro(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new PageCadastro());
+        }
+
+        void OnTappedTarefas(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new PageTarefas());
+        }
+
+        void OnTappedAlertas(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new PageAlertas());
+        }
+
+        void OnTappedConfiguracoes(object sender, EventArgs args)
+        {
+            Navigation.PushModalAsync(new PageConfiguracoes());
+        }
+    }
+}

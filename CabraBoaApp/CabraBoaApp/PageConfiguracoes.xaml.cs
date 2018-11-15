f1d62e6cac2e02db4f767860e9e29cd315558970
Rogerio de Alencar Santos <piurass@yahoo.com.br>
2018-11-15 06:38:59 -0200

    Criação dos grids de crescimento e reprodução nas cores da roda da reprodução

--- CabraBoaApp/CabraBoaApp/PageConfiguracoes.xaml.cs
+++ CabraBoaApp/CabraBoaApp/PageConfiguracoes.xaml.cs
@@ -15,11 +15,23 @@ namespace CabraBoaApp
 		public PageConfiguracoes ()
 		{
 			InitializeComponent ();
-		}
+            ListView.ItemsSource = new List<MenuConfiguracoes>
+            {
+                new MenuConfiguracoes { Nome = "Usuário" },
+                new MenuConfiguracoes { Nome = "Acesso Internet" },
+                new MenuConfiguracoes { Nome = "Alertas" }
+
+            };
+        }
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
         }
+    }
+
+    public class MenuConfiguracoes
+    {
+        public string Nome { get; set; }
     }
 }
